9f3ea301aa8c4fbd111c9334c917037329f7337f
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-12-29 12:43:03 +0600

    popular menu section done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Categories from "./Categories";
+import PopularMenu from "./PopularMenu";
 import Slider from "./Slider";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
         <div>
             <Slider></Slider>
             <Categories></Categories>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
