01748fdc8bd22c3093757bf381b50fd4d28bbff9
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-01-06 04:24:34 +0600

    jwt interceptor used

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,29 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-    baseURL: "http://localhost:5000"
-})
+  baseURL: "http://localhost:5000",
+});
 const useAxiosSecure = () => {
-    return axiosSecure;
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  //   intercepts 401, 403 status
+  axiosSecure.interceptors.response.use(function(response){
+    return response;
+  }, function(error){
+    const status = error.response.status;
+    return Promise.reject(status);
+  })
+
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
